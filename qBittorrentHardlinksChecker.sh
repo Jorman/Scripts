b0445733abee8b9a8c34ddaccf793c813837b1af
Jorman <jorman.franzini@gmail.com>
2022-07-25 18:47:24 +0200

    Added a only_private option for private only check

--- qBittorrentHardlinksChecker.sh
+++ qBittorrentHardlinksChecker.sh
@@ -10,11 +10,14 @@ qbt_username="admin"
 # Password to access to Web UI
 qbt_password="adminadmin"
 
-# Configure here your categories
+# Configure here your categories, comma separated, like -> movie,tv_show
 category_list=''
 
 # Minimum seed time before deletion, expressed in seconds, for example 864000 means 10 days
 min_seeding_time=864000
+
+# Check only private torrents? if not true (lowercase) will check all torrents in given categories
+only_private=true
 ########## CONFIGURATIONS ##########
 
 jq_executable="$(command -v jq)"
@@ -49,9 +52,8 @@ get_torrent_list () {
 }
 
 delete_torrent () {
-	[[ -z "$qbt_cookie" ]] && get_cookie
 	hash="$1"
-	$curl_executable --silent --fail --show-error \
+	echo "$qbt_cookie" | $curl_executable --silent --fail --show-error \
 		--cookie - \
 		--request GET "${qbt_host}:${qbt_port}/api/v2/torrents/delete?hashes=${hash}&deleteFiles=true"
 }
@@ -75,40 +77,62 @@ if [ -n "$category_list" ]; then
 			tor_seeding_time_array=()
 			tor_progress_array=()
 			while read -r single_found; do
-				tor_name_array+=("$single_found")
-				hash=$(echo "$torrents" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .hash')
-				tor_hash_array+=("$hash")
-				path=$(echo "$torrents" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .content_path')
-				tor_path_array+=("$path")
-				seeding_time=$(echo "$torrents" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .seeding_time')
-				tor_seeding_time_array+=("$seeding_time")
-				progress=$(echo "$torrents" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .progress')
-				tor_progress_array+=("$progress")
+				if [[ $only_private == true ]]; then
+					private_check=$(echo "$qbt_cookie" | $curl_executable --silent --fail --show-error --cookie - --request GET "${qbt_host}:${qbt_port}/api/v2/torrents/trackers?hash=$(echo "$torrents" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .hash')" | jq -r '.[0] | .msg | contains("private")')
+
+					if [[ $private_check == true ]]; then
+						tor_name_array+=("$single_found")
+						hash=$(echo "$torrents" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .hash')
+						tor_hash_array+=("$hash")
+						path=$(echo "$torrents" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .content_path')
+						tor_path_array+=("$path")
+						seeding_time=$(echo "$torrents" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .seeding_time')
+						tor_seeding_time_array+=("$seeding_time")
+						progress=$(echo "$torrents" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .progress')
+						tor_progress_array+=("$progress")
+					fi
+				else
+					tor_name_array+=("$single_found")
+					hash=$(echo "$torrents" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .hash')
+					tor_hash_array+=("$hash")
+					path=$(echo "$torrents" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .content_path')
+					tor_path_array+=("$path")
+					seeding_time=$(echo "$torrents" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .seeding_time')
+					tor_seeding_time_array+=("$seeding_time")
+					progress=$(echo "$torrents" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .progress')
+					tor_progress_array+=("$progress")
+				fi
 			done <<< "$torrent_name_list"
 		fi
 
-		for i in "${!tor_name_array[@]}"; do
-			echo "Analyzing torrent -> ${tor_name_array[$i]}"
+		if [ ${#tor_name_array[@]} -gt 0 ]; then
+			for i in "${!tor_name_array[@]}"; do
+				echo "Analyzing torrent -> ${tor_name_array[$i]}"
 
-			if awk "BEGIN {exit !(${tor_progress_array[$i]} < 1)}"; then
-				printf "Torrent incomplete, nothing to do -> %0.3g%%\n" $(awk -v var="${tor_progress_array[$i]}" 'BEGIN{print var * 100}')
-			else
-				if [ "$(ls -l "${tor_path_array[$i]}" | awk '{print $2}')" = "1" ]; then
-					echo "Found 1 hardlinks, checking seeding time:"
-					if [ ${tor_seeding_time_array[$i]} -gt $min_seeding_time ]; then
-						echo "I can delete this torrent, seeding time more than $min_seeding_time seconds"
-						delete_torrent ${tor_hash_array[$i]}
-					else
-						echo "I can not delete this torrent, seeding time not meet -> ${tor_seeding_time_array[$i]}/${min_seeding_time}"
-					fi
+				if awk "BEGIN {exit !(${tor_progress_array[$i]} < 1)}"; then
+					printf "Torrent incomplete, nothing to do -> %0.3g%%\n" $(awk -v var="${tor_progress_array[$i]}" 'BEGIN{print var * 100}')
 				else
-					echo "More than 1 hardlinks found, nothing to do"
+					if [ "$(ls -l "${tor_path_array[$i]}" | awk '{print $2}')" = "1" ]; then
+						echo "Found 1 hardlinks, checking seeding time:"
+						if [ ${tor_seeding_time_array[$i]} -gt $min_seeding_time ]; then
+							echo "I can delete this torrent, seeding time more than $min_seeding_time seconds"
+							delete_torrent ${tor_hash_array[$i]}
+						else
+							echo "I can not delete this torrent, seeding time not meet -> ${tor_seeding_time_array[$i]}/${min_seeding_time}"
+						fi
+					else
+						echo "More than 1 hardlinks found, nothing to do"
+					fi
 				fi
-			fi
-			echo "------------------------------"
-		done
+				echo "------------------------------"
+			done
+		else
+			echo "No torrents found, exiting"
+		fi
 	done
 	echo "Checking complete"
 else
 	echo "Categories list empty"
 fi
+
+#.[0] | .msg | contains("private")
