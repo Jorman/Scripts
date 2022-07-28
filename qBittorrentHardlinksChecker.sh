81de2f8f26d68ee23a4204a9a1febfc30280e10f
Jorman <jorman.franzini@gmail.com>
2022-07-28 07:16:49 +0200

    Orphan torrent and check trackers added

--- qBittorrentHardlinksChecker.sh
+++ qBittorrentHardlinksChecker.sh
@@ -11,7 +11,7 @@ qbt_username="admin"
 qbt_password="adminadmin"
 
 # Configure here your categories, comma separated, like -> movie,tv_show
-category_list=''
+category_list='Serie_Tv,Film'
 
 # Minimum seed time before deletion, expressed in seconds, for example 864000 means 10 days
 min_seeding_time=864000
@@ -46,9 +46,21 @@ get_cookie () {
 
 get_torrent_list () {
 	[[ -z "$qbt_cookie" ]] && get_cookie
-	torrents=$(echo "$qbt_cookie" | $curl_executable --silent --fail --show-error \
+	torrent_list=$(echo "$qbt_cookie" | $curl_executable --silent --fail --show-error \
 		--cookie - \
 		--request GET "${qbt_host}:${qbt_port}/api/v2/torrents/info")
+}
+
+get_tracker_data () {
+	hash="$1"
+	torrent_tracker_data=$(echo "$qbt_cookie" | $curl_executable --silent --fail --show-error \
+		--cookie - \
+		--request GET "${qbt_host}:${qbt_port}/api/v2/torrents/trackers?hash=${hash}")
+}
+
+get_hash_list () {
+	[[ -z "$qbt_cookie" ]] && get_cookie
+	hash_list=$(echo "$torrent_list" | $jq_executable --raw-output '.[] | .hash')
 }
 
 delete_torrent () {
@@ -64,7 +76,7 @@ if [ -n "$category_list" ]; then
 	get_torrent_list
 
 	for j in ${category_list//,/ }; do
-		torrent_name_list=$(echo "$torrents" | $jq_executable --raw-output --arg tosearch "$j" '.[] | select(.category == "\($tosearch)") | .name')
+		torrent_name_list=$(echo "$torrent_list" | $jq_executable --raw-output --arg tosearch "$j" '.[] | select(.category == "\($tosearch)") | .name')
 
 		if [[ -z "$torrent_name_list" ]]; then
 			echo "There's no categories named ${j}"
@@ -78,28 +90,28 @@ if [ -n "$category_list" ]; then
 			tor_progress_array=()
 			while read -r single_found; do
 				if [[ $only_private == true ]]; then
-					private_check=$(echo "$qbt_cookie" | $curl_executable --silent --fail --show-error --cookie - --request GET "${qbt_host}:${qbt_port}/api/v2/torrents/trackers?hash=$(echo "$torrents" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .hash')" | jq -r '.[0] | .msg | contains("private")')
+					private_check=$(echo "$qbt_cookie" | $curl_executable --silent --fail --show-error --cookie - --request GET "${qbt_host}:${qbt_port}/api/v2/torrents/trackers?hash=$(echo "$torrent_list" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .hash')" | $jq_executable --raw-output '.[0] | .msg | contains("private")')
 
 					if [[ $private_check == true ]]; then
 						tor_name_array+=("$single_found")
-						hash=$(echo "$torrents" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .hash')
+						hash=$(echo "$torrent_list" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .hash')
 						tor_hash_array+=("$hash")
-						path=$(echo "$torrents" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .content_path')
+						path=$(echo "$torrent_list" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .content_path')
 						tor_path_array+=("$path")
-						seeding_time=$(echo "$torrents" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .seeding_time')
+						seeding_time=$(echo "$torrent_list" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .seeding_time')
 						tor_seeding_time_array+=("$seeding_time")
-						progress=$(echo "$torrents" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .progress')
+						progress=$(echo "$torrent_list" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .progress')
 						tor_progress_array+=("$progress")
 					fi
 				else
 					tor_name_array+=("$single_found")
-					hash=$(echo "$torrents" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .hash')
+					hash=$(echo "$torrent_list" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .hash')
 					tor_hash_array+=("$hash")
-					path=$(echo "$torrents" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .content_path')
+					path=$(echo "$torrent_list" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .content_path')
 					tor_path_array+=("$path")
-					seeding_time=$(echo "$torrents" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .seeding_time')
+					seeding_time=$(echo "$torrent_list" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .seeding_time')
 					tor_seeding_time_array+=("$seeding_time")
-					progress=$(echo "$torrents" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .progress')
+					progress=$(echo "$torrent_list" | $jq_executable --raw-output --arg tosearch "$single_found" '.[] | select(.name == "\($tosearch)") | .progress')
 					tor_progress_array+=("$progress")
 				fi
 			done <<< "$torrent_name_list"
@@ -130,9 +142,68 @@ if [ -n "$category_list" ]; then
 			echo "No torrents found, exiting"
 		fi
 	done
-	echo "Checking complete"
+	echo "Harklinks check completed"
 else
 	echo "Categories list empty"
 fi
 
-#.[0] | .msg | contains("private")
+echo ""
+
+get_torrent_list
+
+if [ -n "$torrent_list" ]; then
+	echo "Searching for orphan torrent and cleaning up bad trackers url"
+
+	get_hash_list
+
+	tor_name_array=()
+	tor_hashes_array=()
+	orphan_torrents_array=()
+	while read -r single_hashes; do
+		tor_hashes_array+=("$single_hashes")
+		orphan_torrent=$(echo "$qbt_cookie" | $curl_executable --silent --fail --show-error --cookie - --request GET "${qbt_host}:${qbt_port}/api/v2/torrents/trackers?hash=$single_hashes" | $jq_executable --raw-output '.[] | select((.status == 4) and (.num_peers < 0) and (.msg|contains("unregistered"))) | any')
+		if [[ $orphan_torrent == true ]]; then
+			tor_name=$(echo "$qbt_cookie" | $curl_executable --silent --fail --show-error --cookie - --request GET "${qbt_host}:${qbt_port}/api/v2/torrents/info?hashes=$single_hashes" | $jq_executable --raw-output '.[] | .name')
+			tor_name_array+=("$tor_name")
+			orphan_torrents_array+=("$single_hashes")
+		fi
+	done <<< "$hash_list"
+
+	if [ ${#orphan_torrents_array[@]} -gt 0 ]; then
+		for i in "${!orphan_torrents_array[@]}"; do
+			echo "Found orphan torrent -> ${tor_name_array[$i]}, deleting"
+			delete_torrent ${orphan_torrents_array[$i]}
+		done
+	fi
+
+	get_torrent_list
+	get_hash_list
+
+	tor_name_array=()
+	tor_hashes_array=()
+	while read -r single_hashes; do
+		tor_hashes_array+=("$single_hashes")
+		tor_name=$(echo "$qbt_cookie" | $curl_executable --silent --fail --show-error --cookie - --request GET "${qbt_host}:${qbt_port}/api/v2/torrents/info?hashes=$single_hashes" | $jq_executable --raw-output '.[] | .name')
+		tor_name_array+=("$tor_name")
+	done <<< "$hash_list"
+
+	for i in "${!tor_hashes_array[@]}"; do
+		get_tracker_data ${tor_hashes_array[$i]}
+
+		url_list=$(echo "$torrent_tracker_data" | $jq_executable --raw-output '.[] | select((.status == 4) and (.num_peers < 0)) .url')
+
+		if [[ ! -z "$url_list" ]]; then
+			echo "Some problem found on -> ${tor_name_array[$i]}, fixing"
+
+			while read -r single_url; do
+				echo "$qbt_cookie" | $curl_executable --silent --fail --show-error --cookie - --request GET "${qbt_host}:${qbt_port}/api/v2/torrents/removeTrackers?hash=${tor_hashes_array[$i]}&urls=${single_url}"
+			done <<< "$url_list"
+			echo "------------------------------"
+		else
+			continue
+		fi
+	done
+	echo "Done"
+else
+	echo "No torrents founds to check"
+fi
